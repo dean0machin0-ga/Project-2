5f6010c3503c49bbf2166fe0ebe3d30dee8cb125
dean0machin0 <dean0machin00@gmail.com>
2023-11-27 09:48:47 -0800

    Score and standings schema created

--- models/husky.js
+++ models/husky.js
@@ -1,1 +1,29 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const huskySchema = new Schema({
+    Schedule: String,
+    record: Number,
+    pastRecord: Number,
+}, {
+    timestamps: true
+})
+
+const scoreSchema = new Schema({
+    finalScore: Number,
+    quarterlyScore: Number,
+}, {
+    timestamps: true
+})
+
+const standingsSchema = new Schema({
+    pac12Standings: Number,
+    apTop25: Number,
+    cfpTop25: Number
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('huskys', huskySchema)
+module.exports = mongoose.model('huskys', scoreSchema)
+module.exports = mongoose.model('huskys', standingsSchema)
