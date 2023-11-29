8280dc779843dd9ac7f0648ca77b76a635ce630d
dean0machin0 <dean0machin00@gmail.com>
2023-11-29 11:37:01 -0800

    Comment Ui views/games/comments complete

---
+++ controllers/comments.js
@@ -0,0 +1,18 @@
+const Game = require('../models/game')
+
+module.exports = {
+    create
+}
+
+async function create(req, res) {
+    const game = await Game.findById(req.params.id)
+    game.reviews.push(req.body)
+
+    try {
+        await game.save()
+        console.log(game)
+    } catch(err) {
+        console.log(err)
+    }
+    res.redirect(`/games/${game._id}`)
+}
--- models/game.js
+++ models/game.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema({
+    content: { type: String, required: true },
+    gameRating: { type: Number, min: 0, max: 10, default: 5 },
+},
+{ timestamps: true}
+)
+
 const gameSchema = new Schema({
     gameDate: { type: Date, required: true },
     homeTeam: {
@@ -19,40 +26,36 @@ const gameSchema = new Schema({
         "Utah Utes",
         "Oregon St Beavers",
         "Washington St Cougars"
-    ], 
-        default: "Washington Huskys",
-},
+        ], 
+    },
     visitingTeam: {
         type: String,
         enum: [
-            "Washington Huskys",
-            "Boise St Broncos", 
-            "Tulsa Golden Hurricanes",
-            "Michigan St Spartans",
-            "Cal Golden Bears",
-            "Arizona Wildcats",
-            "Oregon Ducks",
-            "Arizona St Sun Devils",
-            "Stanford Cardinal",
-            "USC Trojans",
-            "Utah Utes",
-            "Oregon St Beavers",
-            "Washington St Cougars"
+        "Washington Huskys",
+        "Boise St Broncos", 
+        "Tulsa Golden Hurricanes",
+        "Michigan St Spartans",
+        "Cal Golden Bears",
+        "Arizona Wildcats",
+        "Oregon Ducks",
+        "Arizona St Sun Devils",
+        "Stanford Cardinal",
+        "USC Trojans",
+        "Utah Utes",
+        "Oregon St Beavers",
+        "Washington St Cougars"
         ], 
-        default: "Oregon Ducks",
-},
-
+    },
     huskysStarters: [String],
     oppenentsStarters: [String],
     huskysWin: Boolean,
     finalScore: [String],
     huskysQtrlyScore: [String],
-    oppenentsQtrlyScore: [String]
-    }, 
-    {
+    oppenentsQtrlyScore: [String],
+    comments: [commentSchema]
+    }, {
     timestamps: true
-    }
-)
+    })
 
 const Game = mongoose.model("Game", gameSchema)
 module.exports = Game
