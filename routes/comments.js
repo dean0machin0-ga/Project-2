8f21d1e5114e3f06c88bd7b5d253d907f5d4c2e6
dean0machin0 <dean0machin00@gmail.com>
2023-12-05 08:12:53 -0800

    README updated

--- routes/comments.js
+++ routes/comments.js
@@ -6,24 +6,4 @@ router.post('/games/:id/comments', commentsCtrl.create)
 
 router.delete('/games/:id/comments/:commentid', commentsCtrl.delete)
 
-// router.put('/games/:id', async (req, res) => {
-//     const gameId = req.params.id
-//     const updateData = req.body
-
-//     try {
-//         const updatedGame = await Game.updateResourceInDatabase(gameId, updateData, {
-//         new: true,
-//     }).populate('comments')
-
-//     if (!updatedGame) {
-//         return res.status(404).json({ error: 'Game not found' });
-//     }
-
-//     res.json(updatedGame)
-//     } catch (err) {
-//     console.log(err)
-//     res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// })
-
 module.exports = router
